29b52856960700040ecab2b4c6742e89cc21346a
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-10-08 16:24:54 +0900

    feat

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/cli/go-gh/v2/pkg/api"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-po extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
-}
+	// 1. gh pr list コマンドの実行
+	cmdList := exec.Command("gh", "pr", "list")
+	cmdList.Stdout = os.Stdout
+	cmdList.Stderr = os.Stderr
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+	if err := cmdList.Run(); err != nil {
+		fmt.Println("Error running gh pr list:", err)
+		os.Exit(1)
+	}
+
+	// 2. ユーザーにプルリクエスト番号を入力してもらう
+	fmt.Print("\n\033[32m?\033[0m Type the number of PR to checkout? ... ")
+	reader := bufio.NewReader(os.Stdin)
+	prNumber, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+	prNumber = strings.TrimSpace(prNumber) // 入力の改行や空白を削除
+
+	// 3. 入力されたプルリクエスト番号のチェックアウト処理
+	cmdCheckout := exec.Command("gh", "pr", "checkout", prNumber)
+	cmdCheckout.Stdout = os.Stdout
+	cmdCheckout.Stderr = os.Stderr
+
+	if err := cmdCheckout.Run(); err != nil {
+		fmt.Println("Error checking out PR:", err)
+		os.Exit(1)
+	}
+}
